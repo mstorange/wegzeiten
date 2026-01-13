16dd28adec7bfa00727e3a6bea9820ef637a9213
mstorange <ma.stutz@losinger-marazzi.ch>
2026-01-13 17:52:23 +0100

    Update wegzeiten2.py

--- wegzeiten2.py
+++ wegzeiten2.py
@@ -159,173 +159,176 @@ def plot_folium(gdf, columncolor = None, colormap='RdYlBu_r', hovercolumnlist = 
 startort = st.text_input(label='Startort eingeben')
 endort = st.text_input(label='Zielort eingeben')
 # time = "08:30"
+submitted = st.form_submit_button("Anwenden")
 
-# ------------- Geocoding: von Ortsnamen zu Koordinaten 
-geolocator = Nominatim(user_agent="my_app_nushejan1")
-# store coords (dir(startcoords) für alle methods)
-startcoords = geolocator.geocode(startort)
-startx, starty = startcoords.longitude, startcoords.latitude
-print('gefundene Adresse: ', startcoords.address)
-# st.write('gefundene Adresse: ', startcoords.address)
-endcoords = geolocator.geocode(endort)
-endx, endy = endcoords.longitude, endcoords.latitude
-print('gefundene Adresse: ', endcoords.address)
-# st.write('gefundene Adresse: ', endcoords.address)
-
-
-# ------------ ÖV: Stationen finden
-response_stationfinder_start = requests.get(
-    "http://transport.opendata.ch/v1/locations",
-    params={
-        'x':startx,
-        'y':starty
-    }
-).json()
-response_stationfinder_end = requests.get(
-    "http://transport.opendata.ch/v1/locations",
-    params={
-        'x':endx,
-        'y':endy
-    }
-).json()
-
-def find_next_station(response):
-    #listofstations los
-    los = []
-    for i in response['stations']:
-        if not pd.isna(i['icon']):
-            # print(i)
-            los.append(i)
-    # iterate through options and return train if found, else take the first option with a valid icon
-    trainconnections = []
-    othervehicels = []
-    for s in los:
-        # print('iterate connections: ', s)
-        if s['icon'] == 'train':
-            print('train connection here: ', s)
-            trainconnections.append(s)
+if submitted:
+    if startort and endort:
+        # ------------- Geocoding: von Ortsnamen zu Koordinaten 
+        geolocator = Nominatim(user_agent="my_app_nushejan1")
+        # store coords (dir(startcoords) für alle methods)
+        startcoords = geolocator.geocode(startort)
+        startx, starty = startcoords.longitude, startcoords.latitude
+        print('gefundene Adresse: ', startcoords.address)
+        # st.write('gefundene Adresse: ', startcoords.address)
+        endcoords = geolocator.geocode(endort)
+        endx, endy = endcoords.longitude, endcoords.latitude
+        
+        
+        # ------------ ÖV: Stationen finden
+        response_stationfinder_start = requests.get(
+            "http://transport.opendata.ch/v1/locations",
+            params={
+                'x':startx,
+                'y':starty
+            }
+        ).json()
+        response_stationfinder_end = requests.get(
+            "http://transport.opendata.ch/v1/locations",
+            params={
+                'x':endx,
+                'y':endy
+            }
+        ).json()
+        
+        def find_next_station(response):
+            #listofstations los
+            los = []
+            for i in response['stations']:
+                if not pd.isna(i['icon']):
+                    # print(i)
+                    los.append(i)
+            # iterate through options and return train if found, else take the first option with a valid icon
+            trainconnections = []
+            othervehicels = []
+            for s in los:
+                # print('iterate connections: ', s)
+                if s['icon'] == 'train':
+                    print('train connection here: ', s)
+                    trainconnections.append(s)
+                else:
+                    othervehicels.append(s)
+            
+            if len(trainconnections) != 0:
+                return trainconnections[0] # erste train connection aus der trainliste returnen
+            else:
+                return othervehicels[0]
+        
+        startplatz_json = find_next_station(response=response_stationfinder_start)
+        startplatz = startplatz_json['name']
+        print('Startplatz: ', startplatz)
+        # st.write('Startplatz: ', startplatz)
+        endplatz_json = find_next_station(response=response_stationfinder_end)
+        endplatz = endplatz_json['name']
+        print('Endplatz: ', endplatz)
+        # st.write('Endplatz: ', endplatz)
+        
+        
+        # -------------- ÖV-Wegzeit berechnen
+        # Make a request to get connections
+        response_öv_zeiten = requests.get(
+            "http://transport.opendata.ch/v1/connections",
+            params={
+                "from": startplatz,
+                "to": endplatz,
+                #"via": viaort,
+                "time": zeit
+            }
+        ).json()
+        # Print the whole response to see what it looks like
+        # pprint(response_öv_zeiten['connections'][0])
+        öv_duration = response_öv_zeiten['connections'][0]['duration']
+        
+        # -------------- MIV-Wegzeiten berechnen
+        url = f"http://router.project-osrm.org/route/v1/driving/{startx},{starty};{endx},{endy}"
+        response_miv = requests.get(url, params={
+            "overview": "full",      # Get full geometry
+            "steps": "true",         # Get turn-by-turn instructions
+            "geometries": "geojson"  # Return as GeoJSON
+        }).json()
+        miv_duration = response_miv["routes"][0]["duration"]
+        miv_distance = response_miv["routes"][0]["distance"]
+        print(f"==>> miv_duration: {miv_duration}")
+        # st.write(f"==>> miv_duration: {miv_duration}")
+        # miv_distance = response_miv["routes"][0]["distance"]
+        
+        
+        # ------------- Wegzeiten vergleichen und entsprechende Karte plotten
+        hours, minutes, seconds = map(int, öv_duration[3:].split(':'))
+        öv_duration_time = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        print(f"==>> öv_duration_time: {öv_duration_time}")
+        # st.write(f"==>> öv_duration_time: {öv_duration_time}")
+        miv_duration_time = timedelta(seconds = miv_duration)
+        print(f"==>> miv_duration_time: {miv_duration_time}")
+        # st.write(f"==>> miv_duration_time: {miv_duration_time}")
+        
+        
+        # ------------- Messages ausgeben
+        if öv_duration_time < timedelta(seconds=30*60):
+            print(f'Die Zugfahrt dauert lediglich {öv_duration_time} min und somit kannst du den ÖV nehmen.')
+            fortbewegungsmittel = 'öv'
+        elif öv_duration_time > miv_duration_time:
+            dif = (öv_duration_time-miv_duration_time).seconds/60
+            print(f'Du bist mit dem Auto rund {round(dif, 1)} min schneller.')
+        
+            if öv_duration_time > 1.5*miv_duration_time:
+                faktor = öv_duration_time.seconds/miv_duration_time.seconds
+                print(f'Die Zugfahrt dauert {round(faktor,1)} so lange wie die Autofahrt. Somit darfst du mit dem Auto fahren.')
+                fortbewegungsmittel = 'miv'
+            else:
+                faktor = öv_duration_time.seconds/miv_duration_time.seconds
+                print(f'Die Zugfahrt dauert nur {round(faktor,1)} so lange wie die Autofahrt. Somit musst du den ÖV nehmen, du armes Schwein haha.')
+                fortbewegungsmittel = 'öv'
+        
         else:
-            othervehicels.append(s)
-    
-    if len(trainconnections) != 0:
-        return trainconnections[0] # erste train connection aus der trainliste returnen
+            dif = (miv_duration_time-öv_duration_time).seconds/60
+            print(f'Du bist mit dem ÖV rund {dif} min schneller. Daher nimmst du logischerweise den ÖV.')
+            fortbewegungsmittel = 'öv'
+        
+        # ------------------- Karte ÖV
+        # Teilabschnitte mit den einzelnen Zwischenstopps
+        if fortbewegungsmittel == 'öv':
+            sections = response_öv_zeiten['connections'][0]['sections'] 
+            alle_koordinaten = []
+            for s in sections:
+                print('s: ', s)
+                print('len(s): ', len(s))
+                print('s[journey]: ', s['journey'])
+                if pd.isna(s['journey']):
+                    print('hier musste laufen und zwar von ... nach ...')
+                    print(s['departure']['station']['coordinate'])
+                    print(s['arrival']['station']['coordinate'])
+                    alle_koordinaten.append([s['departure']['station']['coordinate']['y'], s['departure']['station']['coordinate']['x']])
+                    alle_koordinaten.append([s['arrival']['station']['coordinate']['y'], s['arrival']['station']['coordinate']['x']])
+                else: # nicht laufen
+                    print('passList von Nicht-Laufweg: ', s['journey']['passList'])
+                    for station in s['journey']['passList']:
+                        print('Name der Station: ', station['station']['name'])
+                        print('Coords der Station: ', station['station']['coordinate'])
+                        alle_koordinaten.append([station['station']['coordinate']['y'], station['station']['coordinate']['x']])
+            # ÖV Wegkarte
+            lines2 = MultiLineString([alle_koordinaten])
+            övweg = gpd.GeoDataFrame(geometry=[lines2], crs = 'EPSG:4326')
+            route = f'{startort} -> {endort}'
+            dauer = f'{round(öv_duration/60, 1)} min'
+            övweg['Route'] = [route]
+            övweg['Dauer'] = [dauer]
+            m2 = f.plot_folium(övweg, hovercolumnlist=['route'])
+            st_data = st_folium(m2, height = 500, width = 1300, returned_objects=[])
+        
+        # ---------------- Karte MIV
+        else:
+            path = response_miv['routes'][0]['geometry']['coordinates']
+            lines = MultiLineString([path])
+            # lines
+            gdf = gpd.GeoDataFrame(geometry=[lines], crs = 'EPSG:4326')
+            route = f'{startort} -> {endort}'
+            dauer = f'{round(miv_duration/60, 1)} min'
+            dist = f'{round(miv_distance/1000,1)} km'
+            gdf['Route'] = [route]
+            gdf['Dauer'] = [dauer]
+            gdf['Distanz'] = [dist]
+            m = f.plot_folium(gdf, hovercolumnlist=['Route', 'Dauer', 'Distanz'])
+            st_data = st_folium(m, height = 500, width = 1300, returned_objects=[])
     else:
-        return othervehicels[0]
-
-startplatz_json = find_next_station(response=response_stationfinder_start)
-startplatz = startplatz_json['name']
-print('Startplatz: ', startplatz)
-# st.write('Startplatz: ', startplatz)
-endplatz_json = find_next_station(response=response_stationfinder_end)
-endplatz = endplatz_json['name']
-print('Endplatz: ', endplatz)
-# st.write('Endplatz: ', endplatz)
-
-
-# -------------- ÖV-Wegzeit berechnen
-# Make a request to get connections
-response_öv_zeiten = requests.get(
-    "http://transport.opendata.ch/v1/connections",
-    params={
-        "from": startplatz,
-        "to": endplatz,
-        #"via": viaort,
-        "time": zeit
-    }
-).json()
-# Print the whole response to see what it looks like
-# pprint(response_öv_zeiten['connections'][0])
-öv_duration = response_öv_zeiten['connections'][0]['duration']
-
-# -------------- MIV-Wegzeiten berechnen
-url = f"http://router.project-osrm.org/route/v1/driving/{startx},{starty};{endx},{endy}"
-response_miv = requests.get(url, params={
-    "overview": "full",      # Get full geometry
-    "steps": "true",         # Get turn-by-turn instructions
-    "geometries": "geojson"  # Return as GeoJSON
-}).json()
-miv_duration = response_miv["routes"][0]["duration"]
-miv_distance = response_miv["routes"][0]["distance"]
-print(f"==>> miv_duration: {miv_duration}")
-# st.write(f"==>> miv_duration: {miv_duration}")
-# miv_distance = response_miv["routes"][0]["distance"]
-
-
-# ------------- Wegzeiten vergleichen und entsprechende Karte plotten
-hours, minutes, seconds = map(int, öv_duration[3:].split(':'))
-öv_duration_time = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-print(f"==>> öv_duration_time: {öv_duration_time}")
-# st.write(f"==>> öv_duration_time: {öv_duration_time}")
-miv_duration_time = timedelta(seconds = miv_duration)
-print(f"==>> miv_duration_time: {miv_duration_time}")
-# st.write(f"==>> miv_duration_time: {miv_duration_time}")
-
-
-# ------------- Messages ausgeben
-if öv_duration_time < timedelta(seconds=30*60):
-    print(f'Die Zugfahrt dauert lediglich {öv_duration_time} min und somit kannst du den ÖV nehmen.')
-    fortbewegungsmittel = 'öv'
-elif öv_duration_time > miv_duration_time:
-    dif = (öv_duration_time-miv_duration_time).seconds/60
-    print(f'Du bist mit dem Auto rund {round(dif, 1)} min schneller.')
-
-    if öv_duration_time > 1.5*miv_duration_time:
-        faktor = öv_duration_time.seconds/miv_duration_time.seconds
-        print(f'Die Zugfahrt dauert {round(faktor,1)} so lange wie die Autofahrt. Somit darfst du mit dem Auto fahren.')
-        fortbewegungsmittel = 'miv'
-    else:
-        faktor = öv_duration_time.seconds/miv_duration_time.seconds
-        print(f'Die Zugfahrt dauert nur {round(faktor,1)} so lange wie die Autofahrt. Somit musst du den ÖV nehmen, du armes Schwein haha.')
-        fortbewegungsmittel = 'öv'
-
-else:
-    dif = (miv_duration_time-öv_duration_time).seconds/60
-    print(f'Du bist mit dem ÖV rund {dif} min schneller. Daher nimmst du logischerweise den ÖV.')
-    fortbewegungsmittel = 'öv'
-
-# ------------------- Karte ÖV
-# Teilabschnitte mit den einzelnen Zwischenstopps
-if fortbewegungsmittel == 'öv':
-    sections = response_öv_zeiten['connections'][0]['sections'] 
-    alle_koordinaten = []
-    for s in sections:
-        print('s: ', s)
-        print('len(s): ', len(s))
-        print('s[journey]: ', s['journey'])
-        if pd.isna(s['journey']):
-            print('hier musste laufen und zwar von ... nach ...')
-            print(s['departure']['station']['coordinate'])
-            print(s['arrival']['station']['coordinate'])
-            alle_koordinaten.append([s['departure']['station']['coordinate']['y'], s['departure']['station']['coordinate']['x']])
-            alle_koordinaten.append([s['arrival']['station']['coordinate']['y'], s['arrival']['station']['coordinate']['x']])
-        else: # nicht laufen
-            print('passList von Nicht-Laufweg: ', s['journey']['passList'])
-            for station in s['journey']['passList']:
-                print('Name der Station: ', station['station']['name'])
-                print('Coords der Station: ', station['station']['coordinate'])
-                alle_koordinaten.append([station['station']['coordinate']['y'], station['station']['coordinate']['x']])
-    # ÖV Wegkarte
-    lines2 = MultiLineString([alle_koordinaten])
-    övweg = gpd.GeoDataFrame(geometry=[lines2], crs = 'EPSG:4326')
-    route = f'{startort} -> {endort}'
-    dauer = f'{round(öv_duration/60, 1)} min'
-    övweg['Route'] = [route]
-    övweg['Dauer'] = [dauer]
-    m2 = f.plot_folium(övweg, hovercolumnlist=['route'])
-    st_data = st_folium(m2, height = 500, width = 1300, returned_objects=[])
-
-# ---------------- Karte MIV
-else:
-    path = response_miv['routes'][0]['geometry']['coordinates']
-    lines = MultiLineString([path])
-    # lines
-    gdf = gpd.GeoDataFrame(geometry=[lines], crs = 'EPSG:4326')
-    route = f'{startort} -> {endort}'
-    dauer = f'{round(miv_duration/60, 1)} min'
-    dist = f'{round(miv_distance/1000,1)} km'
-    gdf['Route'] = [route]
-    gdf['Dauer'] = [dauer]
-    gdf['Distanz'] = [dist]
-    m = f.plot_folium(gdf, hovercolumnlist=['Route', 'Dauer', 'Distanz'])
-    st_data = st_folium(m, height = 500, width = 1300, returned_objects=[])
+        st.warning("Please enter both start and end locations")
 
